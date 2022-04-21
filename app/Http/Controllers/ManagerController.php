e8811d075405402833ca8d95c4814002a6eeb515
Abdelhady Ahmed <31725200+abdelhadyahmed@users.noreply.github.com>
2022-04-21 13:32:14 +0200

    added time for the manager

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use PHPUnit\Util\Json;
+use Carbon\Carbon;
 
 class ManagerController extends Controller
 {
     public function get_visitors(){
         $visitors = DB::table('visitors')->orderBy('created_at', 'desc')->get();
+        foreach($visitors as $visitor){
+            $visitor->created_at = (new Carbon($visitor->created_at))->format('h:ia F d, Y ');
+        }
         return json_encode($visitors);
     }
     public function delete($id){
